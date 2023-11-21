d0fe5a5c9a99b492e5cc598dc3c59db5647b2f7d
Matija Bogdanovic <matijabog45@gmail.com>
2023-11-22 00:06:11 +0100

    aa

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -12,9 +12,9 @@ class RegisterRequest extends AbstractRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|between:2,100',
+            'name' => 'required|string|between:6,100',
             'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:9',
         ];
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
